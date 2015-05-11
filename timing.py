e7afb8523e14ddf1af4c4ad67fc2422ae080a11f
Anthony LaTorre <tlatorre@uchicago.edu>
2015-05-10 20:25:37 -0500

    update timing

--- timing.py
+++ timing.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
 
 def get_times(y, fraction=0.4):
     """
@@ -10,28 +11,28 @@ def get_times(y, fraction=0.4):
     waveform.
     """
     # samples below threshold
-    mask1 = y < np.min(y,axis=-1)[:,np.newaxis]*fraction
+    mask1 = y > np.min(y,axis=-1)[:,np.newaxis]*fraction
     # samples before the minimum
     mask2 = np.arange(y.shape[1]) < np.argmin(y,axis=-1)[:,np.newaxis]
 
     # right side of threshold crossing
-    r = np.argmax(mask1 & mask2, axis=-1)
+    r = y.shape[1] - np.argmax((mask1 & mask2)[:,::-1], axis=-1)
     r[r == 0] = 1
-    # left side
-    l = r -1
+    r[r == y.shape[1]] = y.shape[1] - 1
+    l = r - 1
 
     yl = y[np.arange(y.shape[0]),l]
     yr = y[np.arange(y.shape[0]),r]
 
     return (np.min(y,axis=-1)*fraction - yl)/(yr-yl) + l
 
-def fft_filter(y, cutoff=500e6, sample_rate=2e9):
+def fft_filter(y, dt, cutoff=500e6):
     """
     Filter the array `y` by removing frequency components above
     `cutoff` in Hz.
     """
     out = np.fft.rfft(y)
-    freq = np.fft.rfftfreq(y.shape[1], d=1.0/sample_rate)
+    freq = np.fft.rfftfreq(y.shape[1], d=dt)
     out[:,freq > cutoff] = 0
     return np.fft.irfft(out)
 
@@ -42,31 +43,31 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', dest='output')
+    parser.add_argument('-o', dest='output', default='out.txt')
     parser.add_argument('filenames', nargs='+', help='input files')
+    parser.add_argument('-c', '--chunk', type=int, default=100000)
     args = parser.parse_args()
 
-    t2 = []
+    t = []
     for filename in args.filenames:
         print filename
         with h5py.File(filename) as f:
             start = time.time()
-            y2 = f['c2'][:]
+            for i in range(0, f['c1'].shape[0], args.chunk):
+                y1 = f['c1'][i:i+args.chunk]
+                y2 = f['c2'][i:i+args.chunk]
+                mask = np.min(y2,axis=-1) < -1000
+                dt = float(f['c1'].attrs['XINCR'])*1e9
+                y1 = fft_filter(y1[mask], dt)
+                y2 = fft_filter(y2[mask], dt)
+                t1 = get_times(-y1)*dt
+                t2 = get_times(y2)*dt
+                t.extend(t2 - t1)
             stop = time.time()
-            print '%.2f seconds to open file' % (stop-start)
-            mask = np.min(y2,axis=-1) < -1000
-            y1 = -f['c1'][:][mask,:]
-            y1 = fft_filter(y1)
-            y2 = fft_filter(y2[mask])
-            t1 = get_times(y1)
+            print '%.2f seconds to process file' % (stop-start)
 
-            dt = float(f['c1'].attrs['XINCR'])*1e9
-            t2.extend((get_times(y2) - t1)*dt) # convert to ns
     if args.output:
-        np.savetxt(args.output, t2)
-    plt.hist(t2, bins=1024, range=[-100,200],log=True)
-    plt.xlabel('Time (ns)')
-    plt.show()
+        np.savetxt(args.output, t)
 
 if __name__ == '__main__':
     main()
